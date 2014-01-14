db7fd071a602932e6c69db0a1e287a2581619ecf
SerenityS2 <bs4213@naver.com>
2014-01-14 10:56:47 +0900

    etc..

--- clean_kernel.sh
+++ clean_kernel.sh
@@ -7,10 +7,10 @@
 # 2011-10-24 effectivesky : modified
 # 2010-12-29 allydrop     : created
 ###############################################################################
-export ARCH=arm
-export PATH=~/temasek/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
-export CROSS_COMPILE=~/temasek/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 
+#export PATH=$(pwd)/toolchain/arm-eabi-4.6/bin:$PATH
+export PATH=~/ef39s-jb/toolchain/linaro-4.7/bin:$PATH
+export CROSS_COMPILE=~/ef39s-jb/toolchain/linaro-4.7/bin/arm-eabi-
 make mrproper
 make O=./obj/KERNEL_OBJ/ clean
 if [ -f ./zImage ]
